92dbb27e7c0eb31cd9c9ceaf8088a8da84678c13
fantasia <>
2010-02-01 03:22:40 +0000

    Added Eff. Plots

--- analyses/WZcode/WZAnalysisV3/WZAnalyzer/test/wprime_wz_macros/MakePlots.C
+++ analyses/WZcode/WZAnalysisV3/WZAnalyzer/test/wprime_wz_macros/MakePlots.C
@@ -10,83 +10,29 @@
 #include "TMath.h"
 
 void DrawandSave(TFile* fin, string title);
+void PlotEff(TFile* fin, string title);
 
 void
 MakePlots(){  
-   
+    const int Nplots = 5;
+
     TFile *fin = TFile::Open("Wprime_analysis.root");
-    string title; 
-    ///////////
-    title = "hHt_muon";        DrawandSave(fin,title);
-    title = "hWZInvMass_elec"; DrawandSave(fin,title);
-    title = "hWZInvMass_muon"; DrawandSave(fin,title);
-    title = "hWpt_muon";       DrawandSave(fin,title);
-    title = "hZpt_muon";       DrawandSave(fin,title);
+    string title[Nplots]; 
+    string efftitle[3];
+
+    title[0] = "hHt_muon";       
+    title[1] = "hWZInvMass_elec";
+    title[2] = "hWZInvMass_muon";
+    title[3] = "hWpt_muon";      
+    title[4] = "hZpt_muon";      
+
+    efftitle[0] = "hEffAbs";
+    efftitle[1] = "hEffRel";
+    efftitle[2] = "hNumEvts";
+
+    for(int i=0;i<Nplots;++i) DrawandSave(fin,title[i]);
     
-    /*
-    h1 = (TH1F*) fin->Get("wprime400/hHt_muon");
-    h2 = (TH1F*) fin->Get("wzjj/hHt_muon");
-
-    sum = h1->Integral(); h1->Scale(1./sum);
-    sum = h2->Integral(); h2->Scale(1./sum);
-			      
-    h1->SetLineColor(kRed);  h1->Draw();
-    h2->SetLineColor(kBlue); h2->Draw("same");
-
-    c1->SaveAs("hHt_muon.eps");
-
-    ////////////
-    
-    h1 = (TH1F*) fin->Get("wprime400/hWZInvMass_muon");
-    h2 = (TH1F*) fin->Get("wzjj/hWZInvMass_muon");
-
-    sum = h1->Integral(); h1->Scale(1./sum);
-    sum = h2->Integral(); h2->Scale(1./sum);
-			      
-    h1->SetLineColor(kRed);  h1->Draw();
-    h2->SetLineColor(kBlue); h2->Draw("same");
-
-    c1->SaveAs("hWZInvMass_muon.eps");
-
-    ////////////
-
-    h1 = (TH1F*) fin->Get("wprime400/hWZInvMass_elec");
-    h2 = (TH1F*) fin->Get("wzjj/hWZInvMass_elec");
-
-    sum = h1->Integral(); h1->Scale(1./sum);
-    sum = h2->Integral(); h2->Scale(1./sum);
-			      
-    h1->SetLineColor(kRed);  h1->Draw();
-    h2->SetLineColor(kBlue); h2->Draw("same");
-
-    c1->SaveAs("hWZInvMass_elec.eps");
-
- ////////////
-
-    h1 = (TH1F*) fin->Get("wprime400/hWpt_muon");
-    h2 = (TH1F*) fin->Get("wzjj/hWpt_muon");
-
-    sum = h1->Integral(); h1->Scale(1./sum);
-    sum = h2->Integral(); h2->Scale(1./sum);
-			      
-    h1->SetLineColor(kRed);  h1->Draw();
-    h2->SetLineColor(kBlue); h2->Draw("same");
-
-    c1->SaveAs("hWpt_muon.eps");
-
- ////////////
-
-    h1 = (TH1F*) fin->Get("wprime400/hZpt_muon");
-    h2 = (TH1F*) fin->Get("wzjj/hZpt_muon");
-
-    sum = h1->Integral(); h1->Scale(1./sum);
-    sum = h2->Integral(); h2->Scale(1./sum);
-			      
-    h1->SetLineColor(kRed);  h1->Draw();
-    h2->SetLineColor(kBlue); h2->Draw("same");
-
-    c1->SaveAs("hZpt_muon.eps");
-    */
+    for(int i=0;i<3;++i) PlotEff(fin,efftitle[i]);
 }
 
 
@@ -127,6 +73,35 @@ DrawandSave(TFile* fin, string title){
 
     max = TMath::Max(max1,max2);
     h1->SetMaximum(max);
+
+    h1->SetLineColor(kRed);  h1->Draw();
+    h2->SetLineColor(kBlue); h2->Draw("same");
+
+    c1.SaveAs(filename.c_str());
+}
+
+void
+MakePlots2(){
+    
+}
+
+void
+PlotEff(TFile* fin, string title){
+    string dir1 = "wprime400";
+    string dir2 = "wzjj";
+
+    string h1_name = dir1 + "/" + title;
+    string h2_name = dir2 + "/" + title;
+    string filename = title + ".eps";
+
+    TCanvas c1;
+    TH1F *h1,*h2;
+        
+    h1 = (TH1F*) fin->Get(h1_name.c_str());
+    h2 = (TH1F*) fin->Get(h2_name.c_str());
+
+    h1->SetStats(kFALSE);
+    h2->SetStats(kFALSE);
 
     h1->SetLineColor(kRed);  h1->Draw();
     h2->SetLineColor(kBlue); h2->Draw("same");
